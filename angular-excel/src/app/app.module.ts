687c890c835a441d79951fe6bd843e70d842be79
Carl Vuorinen <carl.vuorinen@gmail.com>
2017-05-25 21:25:41 +0300

    Install ngParser & add functionality to spreadsheet service

--- angular-excel/src/app/app.module.ts
+++ angular-excel/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ExcelComponent } from './excel/excel.component';
 import { CellComponent } from './cell/cell.component';
-
+import { SpreadsheetService } from "./spreadsheet.service";
 
 @NgModule({
   declarations: [
@@ -14,9 +14,11 @@ import { CellComponent } from './cell/cell.component';
     CellComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [SpreadsheetService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-excel/src/typings.d.ts
+++ angular-excel/src/typings.d.ts
@@ -3,3 +3,8 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+/* ng-parser type definitions */
+declare var ngParser: (expression: string, options?: Object) => Expression;
+
+declare type Expression = (scope?: Object) => any;
