1a98b283fc7c17dbb687041794f222c9fdaa0311
Carl Vuorinen <carl.vuorinen@gmail.com>
2017-05-25 11:32:14 +0300

    Clean up generated app component spec

--- angular-excel/src/app/app.component.spec.ts
+++ angular-excel/src/app/app.component.spec.ts
@@ -1,6 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let element: HTMLElement;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -8,20 +13,25 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    element = fixture.debugElement.nativeElement;
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+  });
+
+  it('should create the app', async(() => {
+    expect(component).toBeTruthy();
+  }));
+
+  it(`should have as title 'app'`, async(() => {
+    expect(component.title).toEqual('app');
+  }));
+
+  it('should render title in a h1 tag', async(() => {
+    const titleElement = element.querySelector('h1');
+
+    expect(titleElement.textContent).toContain('Welcome to app!');
   }));
 });
