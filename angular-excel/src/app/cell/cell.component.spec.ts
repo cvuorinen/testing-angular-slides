1759d2e859c30ea3df9405da7ab3b26ef03aff84
Carl Vuorinen <carl.vuorinen@gmail.com>
2017-05-28 21:50:39 +0300

    Test cell component with mock service

--- angular-excel/src/app/cell/cell.component.spec.ts
+++ angular-excel/src/app/cell/cell.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, fakeAsync, tick, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from "@angular/forms";
 
 import { CellComponent } from './cell.component';
@@ -7,6 +7,7 @@ import { SpreadsheetService } from "app/spreadsheet.service";
 describe('CellComponent', () => {
   let component: CellComponent;
   let fixture: ComponentFixture<CellComponent>;
+  let service: SpreadsheetService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,9 +22,32 @@ describe('CellComponent', () => {
     fixture = TestBed.createComponent(CellComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    service = TestBed.get(SpreadsheetService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should evaluate with SpreadsheetService when formula value changes', fakeAsync(() => {
+    const value = 'foo';
+    spyOn(service, 'evaluate');
+
+    component.formula.setValue(value);
+    tick(500);
+
+    expect(service.evaluate).toHaveBeenCalledWith(value);
+  }));
+
+  it('should expose evaluated result in value$', fakeAsync(() => {
+    let currentValue;
+    const result = 'bar';
+    spyOn(service, 'evaluate').and.returnValue(result);
+
+    component.value$.subscribe(value => currentValue = value);
+    component.formula.setValue('foo');
+    tick(500);
+
+    expect(currentValue).toBe(result);
+  }));
 });
