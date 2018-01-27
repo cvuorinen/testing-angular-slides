be778c96992d6ddab1c470278a3da8304b0df490
Carl Vuorinen <carl.vuorinen@gmail.com>
2018-01-27 19:23:02 +0200

    Upgrade to Angular 5 & angular-cli 1.6

--- angular-excel/e2e/app.e2e-spec.ts
+++ angular-excel/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AngularExcelPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('angular-excel App', () => {
-  let page: AngularExcelPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AngularExcelPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
--- angular-excel/e2e/app.po.ts
+++ angular-excel/e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class AngularExcelPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
